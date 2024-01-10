5f79ca136bf3d50a0e3307fae2849b196ec4dc69
Ben-Aroh <bennetharoh@gmail.com>
2024-01-10 09:43:18 +0000

    Add remote target

--- target/s3/main.tf
+++ target/s3/main.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "s3" {
+    source = "github.com/Ben-Aroh/remote_modue_base//module/s3?ref=10.01.2024"
+}
